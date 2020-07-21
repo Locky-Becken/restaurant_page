194c39620452827d4a4b2ab2ca0eee76ef389661
João Pedro <number11.metamorph@gmail.com>
2020-07-21 14:46:17 -0300

    "update"

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function switchTab(e) {
     if (e.target.id === 'nav-div') return;
 
     if (e.target.id === 'nav-Menu') {
-        content.removeChild(content.lastChild); // remove o item anterior e exibite o target clicado
+        content.removeChild(content.lastChild); // remove o item anterior e exibe o target clicado
         showMenu();
     } else if (e.target.id === 'nav-Home'){
         content.removeChild(content.lastChild);
